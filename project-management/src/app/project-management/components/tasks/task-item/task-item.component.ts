c649640a2f2589fe407260276bce46eeaa3e2419
EkaterinaMash <ekaterina.mashinskayaa@gmail.com>
2023-07-29 14:04:14 +0300

    feat: add task delete functionality

--- project-management/src/app/project-management/components/tasks/task-item/task-item.component.ts
+++ project-management/src/app/project-management/components/tasks/task-item/task-item.component.ts
@@ -1,12 +1,50 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {TaskType} from "../../../../shared/types/task-type.model";
+import {TasksService} from "../../../../shared/services/tasks.service";
+import {ModalComponent} from "../../../../shared/components/modal/modal.component";
+import {MatDialog} from "@angular/material/dialog";
+import {removeTask} from "../../../../store/actions/column.actions";
+import {GeneralState} from "../../../../store/state.model";
+import {Store} from "@ngrx/store";
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.scss']
 })
-export class TaskItemComponent {
-@Input() task: TaskType | undefined;
-@Input() columnId: string;
+export class TaskItemComponent implements OnInit {
+  @Input() task: TaskType | undefined;
+  @Input() columnId: string;
+  @Output() deleteTaskEvent = new EventEmitter<TaskType>;
+  boardId: string;
+  taskId: string;
+  delete: boolean = false;
+
+  constructor(private tasksService: TasksService,
+              private dialog: MatDialog,
+              private store: Store<GeneralState>) {
+  }
+
+  ngOnInit() {
+    this.boardId = this.task.boardId;
+    this.taskId = this.task._id;
+  }
+
+  deleteTask() {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      data: {title: this.task.title, item: 'task'}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      this.delete = result.data;
+      if (this.delete) {
+        this.tasksService
+          .deleteTask(this.boardId, this.columnId, this.taskId)
+          .subscribe((data: TaskType) => {
+            this.store.dispatch(removeTask({removedTask: data}));
+          });
+        this.deleteTaskEvent.emit(this.task);
+        this.delete = false;
+      }
+    })
+  }
 }
